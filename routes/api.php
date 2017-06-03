5dac7449b5116195a60bca16e3386710d3a5efff
ibrahimhasann7@gmail.com <ibrahimhasann7@gmail.com>
2017-06-03 23:44:53 +0700

    asdf

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/',funtion(){
+Route::get('/',function(){
     return response( 'welcome to change.org api', 200 );
 });
 
